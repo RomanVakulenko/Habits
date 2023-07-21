addb15bad457a6ad8b0491923dd1ec5431bd8728
RomanVakulenko <vakula_48@mail.ru>
2023-07-21 10:17:10 +0300

    statusCell composition is ready (Width and Height SETS in collectionView DelegateFlowLayout!)

--- Habits/HabitsViewController.swift
+++ Habits/HabitsViewController.swift
@@ -9,28 +9,121 @@ import UIKit
 
 final class HabitsViewController: UIViewController {
 
+    private lazy var collectionView: UICollectionView = {
+        let collectionLayout = UICollectionViewFlowLayout()
+        collectionLayout.scrollDirection = .vertical
+
+        let collection = UICollectionView(frame: .zero, collectionViewLayout: collectionLayout)
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        collection.backgroundColor = #colorLiteral(red: 0.9494450688, green: 0.9487820268, blue: 0.9670705199, alpha: 1)
+        collection.register(ProgressCollectionViewCell.self, forCellWithReuseIdentifier: ProgressCollectionViewCell.identifier)
+        collection.delegate = self
+        collection.dataSource = self
+        return collection
+    }()
+
+//MARK: - lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        layout()
+        navigationController?.navigationBar.largeContentTitle = "Cегодня"
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(
+        navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(
             image: UIImage(systemName: "plus"),
             style: .plain,
             target: self,
-            action: #selector(addHabit))
+            action: #selector(addHabit)
+        )
     }
 
+
+//MARK: - methods
+
+    private func layout() {
+        view.addSubview(collectionView)
+
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+    }
 
     @objc func addHabit() {
         let addOrEditHabitVC = AddOrEditHabitVC()
         addOrEditHabitVC.modalTransitionStyle = .coverVertical
         addOrEditHabitVC.modalPresentationStyle = .fullScreen
-        addOrEditHabitVC.view.backgroundColor = UIColor(red: 242, green: 242, blue: 247, alpha: 1)
+        addOrEditHabitVC.view.backgroundColor = UIColor(red: 0.949, green: 0.949, blue: 0.969, alpha: 1)
 
         present(addOrEditHabitVC, animated: true)
     }
     
+}
+
+//MARK: - UICollectionViewDataSource
+
+extension HabitsViewController: UICollectionViewDataSource {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        2
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        var numberOfItems = 0
+        if section == 0 {
+            numberOfItems = 1
+        } else {
+            numberOfItems = HabitsStore.shared.habits.count
+        }
+        return numberOfItems
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: ProgressCollectionViewCell.identifier, for: indexPath) as? ProgressCollectionViewCell else { return UICollectionViewCell()}
+        cell.setupProgressCell()
+        //this needs to create delegate
+//        cell.delegate = self
+//        cell.<#methodGivesIndexPath#>(indexPath)
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+
+    }
+}
+
+
+//MARK: - UICollectionViewDelegateFlowLayout
+
+extension HabitsViewController: UICollectionViewDelegateFlowLayout {
+
+    private var inset: CGFloat { return 16 }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.bounds.width - inset * 2
+        var height = 0.0
+        if indexPath == [0,0] {
+            height = 60
+        } else {
+            height = 130
+        }
+        return CGSize(width: width, height: height)
+    }
+    //отступы по периметру дисплея
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 22, left: inset, bottom: 0, right: inset)
+    }
+    //отступы между рядами-строками для вертикальной коллекции
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+       12
+    }
+    // между элементами в 1 строке - вертикальная коллекции, в 1 столбце-горизонтКоллекц
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        inset
+//    }
 }
---
+++ Habits/ProgressCollectionViewCell.swift
@@ -0,0 +1,97 @@
+//
+//  ProgressCollectionViewCell.swift
+//  Habits
+//
+//  Created by Roman Vakulenko on 18.07.2023.
+//
+
+import UIKit
+
+final class ProgressCollectionViewCell: UICollectionViewCell {
+
+    private let whiteView: UIView = {
+        let whiteView = UIView()
+        whiteView.translatesAutoresizingMaskIntoConstraints = false
+        whiteView.backgroundColor = .white
+        whiteView.layer.cornerRadius = 8
+        return whiteView
+    }()
+
+    private let progressView: UIProgressView = {
+        let progress = UIProgressView(progressViewStyle: .default)
+        progress.translatesAutoresizingMaskIntoConstraints = false
+        progress.progress = 0
+        progress.observedProgress?.totalUnitCount = 100
+        progress.progressTintColor = #colorLiteral(red: 0.6309858561, green: 0.08523046225, blue: 0.8001720309, alpha: 1)
+        progress.trackTintColor = #colorLiteral(red: 0.8487350345, green: 0.8487350941, blue: 0.8487350345, alpha: 1)
+        return progress
+    }()
+
+    private let labelView: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = UIColor(red: 0.557, green: 0.557, blue: 0.576, alpha: 1)
+        label.text = "Все получится!"
+        label.font = UIFont(name: "SFProText-Semibold", size: 13)
+        return label
+    }()
+
+    private lazy var percentLabelView: UILabel = {
+        let progressLabel = UILabel()
+        progressLabel.translatesAutoresizingMaskIntoConstraints = false
+        progressLabel.textColor = UIColor(red: 0.557, green: 0.557, blue: 0.576, alpha: 1)
+        progressLabel.font = UIFont(name: "SFProText-Semibold", size: 13)
+        progressLabel.textAlignment = .right
+        return progressLabel
+    }()
+    
+
+    //MARK: - init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        layout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    //MARK: - setup
+    func setupProgressCell(){
+        progressView.progress = 0.5 //HabitsStore.shared.todayProgress
+        percentLabelView.text = String(HabitsStore.shared.todayProgress * 100) + "%"
+    }
+
+//    override func prepareForReuse() {
+//        progressLabelView.text = nil
+//        progressView.progress = nil
+//    }
+
+    //MARK: - layout
+    private func layout() {
+        [progressView, labelView, percentLabelView].forEach { whiteView.addSubview($0) }
+        contentView.addSubview(whiteView)
+
+        NSLayoutConstraint.activate([
+            whiteView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            whiteView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            whiteView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            whiteView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+
+            labelView.leadingAnchor.constraint(equalTo: whiteView.leadingAnchor, constant: 12),
+            labelView.topAnchor.constraint(equalTo: whiteView.topAnchor, constant: 10),
+            labelView.heightAnchor.constraint(equalToConstant: 18),
+
+            percentLabelView.trailingAnchor.constraint(equalTo: whiteView.trailingAnchor, constant: -12),
+            percentLabelView.topAnchor.constraint(equalTo: whiteView.topAnchor, constant: 10),
+            percentLabelView.heightAnchor.constraint(equalToConstant: 18),
+
+            progressView.leadingAnchor.constraint(equalTo: whiteView.leadingAnchor, constant: 12),
+            progressView.trailingAnchor.constraint(equalTo: whiteView.trailingAnchor, constant: -12),
+            progressView.topAnchor.constraint(equalTo: percentLabelView.bottomAnchor, constant: 10),
+            progressView.bottomAnchor.constraint(equalTo: whiteView.bottomAnchor, constant: -15),
+            progressView.heightAnchor.constraint(equalToConstant: 7)
+        ])
+
+    }
+}
